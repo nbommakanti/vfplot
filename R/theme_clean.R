9be95e7db6b32d8941d611d12882611c87f6cce5
Nikhil Bommakanti <nikhil.bommakanti@gmail.com>
2019-03-04 12:48:30 -0500

    rename

--- R/theme_clean.R
+++ R/theme_clean.R
@@ -1,20 +1,30 @@
-#' Theme for 24-2 heatmaps
+#' Theme for clean plots
+#' Use for 24-2 heatmaps, histograms, etc.
 #'
 #' @return NULL
 #' @import ggplot2
 #' @export
 #'
 #' @examples
-#' p + theme_heatmap()
-theme_heatmap <- function (...) {
-    theme_classic(base_family = "Roboto", base_size = 14) +
+#' p + theme_clean()
+theme_clean <- function (base_family = "Helvetica", base_size = 14) {
+    theme_classic(
+        base_family = base_family,
+        base_size = base_size
+    ) %+replace%
         theme(
+            # Large title and smaller subtitle
             plot.title = element_text(size = 18, face = "bold"),
             plot.subtitle = element_text(size = 12),
+
+            # Hide y axis line and ticks
             axis.line.y = element_blank(),
-            panel.grid.major.y = element_line(size = 0.1, linetype = 3,
-                                              color = colorscale_columbia["lightgray"]),
-            ...)
+            axis.ticks.y = element_blank(),
+
+            # Add dashed y-axis line
+            panel.grid.major.y = element_line(size = 0.2,
+                                              linetype = 3,
+                                              color = colorscale_columbia["lightgray"]))
 }
 
 
